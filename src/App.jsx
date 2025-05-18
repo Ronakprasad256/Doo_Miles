0d137de138c94e1b01c91a2d9c9bfe071ed8e7aa
Ronakprasad256 <ronakpd07@gmail.com>
2025-05-18 10:50:30 +0530

    Car list added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Aos from 'aos';
 import "aos/dist/aos.css"
 import About from './components/About/About';
 import Services from './components/Services/Services';
+import CarList from './components/CarList/CarList';
 
 const App = () => {
 
@@ -45,6 +46,7 @@ const App = () => {
       <Hero theme={theme}  />
       <About/>
       <Services/>
+      <CarList />
     </div>
   )
 }
---
+++ src/components/CarList/CarList.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import whiteCar from "/bmw.png"
+import scor from "/scor.png"
+import scorpio from "/scorpio.png"
+
+const CarListData = [
+    {
+        name: "BMW UX",
+        price: 100,
+        image: whiteCar,
+        aosDelay: "0",
+    },
+    {
+        name: "KIA UX",
+        price: 140,
+        image: scor,
+        aosDelay: "500",
+    },
+    {
+        name: "BMW UX",
+        price: 120,
+        image: scorpio,
+        aosDelay: "1000",
+    },
+]
+const CarList = () => {
+    return (
+        <div className='pb-24 pt-12
+    bg-white dark:bg-dark
+    dark:text-white'>
+            <div className="container">
+                {/* heading */}
+                <h1
+                    data-aos="fade-up"
+                    className='text-3xl 
+            sm:text-4xl
+            font-semibold font-serif mb-3'
+                >Lorem ipsum dolor.
+                </h1>
+                <p data-aos="fade-up" 
+                className='text-sm pb-10'>
+                Lorem, ipsum dolor sit,
+                amet consectetur
+                adipisicing elit. Ducimus, facilis!</p>
+                {/* car listing cards */}
+                <div>
+                    <div className='grid 
+                grid-cols-1
+                sm:grid-cols-2 
+                md:grid-cols-3 gap-16'>
+                        {
+                            CarListData.map((data) => {
+                                return(<div className='space-y-3 border-2
+                                border-gray-300 hover:border-primary cursor-pointer
+                                p-3 rounded-xl relative group
+                                '>
+                                    <div className='w-full h-[120px]'>
+                                        <img className='w-full h-[120px] object-contain
+                                        sm:translate-x-8 group-hover:translate-x-24
+                                        duration-700
+                                        ' src={data.image} alt="" />
+                                    </div>
+                                    <div className='space-y-2'>
+                                        <h1 className=' text-primary
+                                        font-semiboldsem
+                                        '>{data.name}</h1>
+                                        <div className='flex
+                                        justify-between items-center
+                                        text-xl font-semibold
+                                        '>
+                                            <p>${data.price}/day</p>
+                                            <a href="#">Details</a>
+                                        </div>
+                                    </div>
+                                    <p className='text-xl font-semibold
+                                    absolute top-0 left-3
+                                    '>12Km</p>
+                                </div>
+                                );
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CarList;
